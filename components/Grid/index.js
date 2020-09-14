106d7cd6d8f5d275bc709f1e595842bdad9c2e82
bretmorris <bret@useallfive.com>
2020-09-14 15:30:38 -0600

    add global grid component

---
+++ components/Grid/index.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+import styles from './index.module.scss'
+import { gg } from 'css-grid-guides'
+import { useEffect } from 'react'
+import classNames from 'classnames'
+
+// global grid.
+// please adjust the css properties to customize for your project.
+const Grid = ({ children, guides }) => {
+  useEffect(() => {
+    gg()
+  }, [guides])
+  return <div className={classNames({ [styles.grid]: true, grid: guides })}>{children}</div>
+}
+
+Grid.propTypes = {
+  children: PropTypes.node,
+  guides: PropTypes.bool,
+}
+
+export default Grid
