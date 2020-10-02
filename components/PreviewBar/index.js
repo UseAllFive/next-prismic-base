b31ddf92b862b4151c28b777ea7dd6c5ecf80175
bretmorris <bret@useallfive.com>
2020-10-02 13:58:43 -0600

    formatting fix

--- components/PreviewBar/index.js
+++ components/PreviewBar/index.js
@@ -4,8 +4,7 @@ export default function PreviewBar() {
   return (
     <div className={styles.bar}>
       This is page is a preview.&nbsp;
-      <a href="/api/exit-preview">Click here</a>&nbsp;
-      to exit preview mode.
+      <a href="/api/exit-preview">Click here</a>&nbsp; to exit preview mode.
     </div>
   )
 }
