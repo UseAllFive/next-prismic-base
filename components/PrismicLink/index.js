561d8e288b89f83517d783923576276f9e932466
bretmorris <bret@useallfive.com>
2020-10-23 14:54:39 -0600

    update prismiclink component

--- components/PrismicLink/index.js
+++ components/PrismicLink/index.js
@@ -1,29 +1,59 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
 import { linkResolver, hrefResolver } from 'lib/resolvers'
 import prismicLinkShape from 'shapes/prismic/link'
+import styles from './index.module.scss'
 
-const PrismicLink = ({ className, link, children }) => {
+const PrismicLink = ({ className, activeClassName, link, children }) => {
   const { link_type, url, target } = link
-  const rel = target === '_blank' ? 'noreferrer' : null
-  return (
-    <React.Fragment>
-      {link_type === 'Document' ? (
-        <Link href={hrefResolver(link)} as={linkResolver(link)}>
-          <a className={className}>{children}</a>
-        </Link>
-      ) : (
-        <a className={className} href={url} target={target} rel={rel}>
+  const DocumentLink = () => {
+    const { asPath } = useRouter()
+    const asPathWithSlash = `${asPath}/`
+    const href = hrefResolver(link)
+    const as = linkResolver(link)
+    const isActive = asPathWithSlash === href || asPathWithSlash === as
+    const attrs = { as, href }
+    return (
+      <Link {...attrs}>
+        <a
+          className={classNames(styles.link, className, {
+            [activeClassName]: isActive,
+            [styles.linkActive]: isActive,
+          })}>
           {children}
         </a>
-      )}
+      </Link>
+    )
+  }
+  const RegularLink = () => {
+    return (
+      <a
+        className={classNames(styles.link, className)}
+        href={url}
+        target={target}
+        rel={target === '_blank' && 'noreferrer'}>
+        {children}
+      </a>
+    )
+  }
+  const NoLink = () => {
+    return <span className={className}>{children}</span>
+  }
+  return (
+    <React.Fragment>
+      {link_type === 'Document' && <DocumentLink />}
+      {link_type === 'Web' && <RegularLink />}
+      {link_type === 'Any' && <NoLink />}
     </React.Fragment>
   )
 }
 
 PrismicLink.propTypes = {
   className: PropTypes.string,
+  activeClassName: PropTypes.string,
   link: prismicLinkShape.isRequired,
   children: PropTypes.node,
 }
