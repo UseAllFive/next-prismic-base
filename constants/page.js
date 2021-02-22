4479094220153f8646ccd5da7e40886708662663
suhmantha1 <suhmantha1@gmail.com>
2021-02-22 13:53:53 -0500

    page slug formation

---
+++ constants/page.js
@@ -0,0 +1,6 @@
+import { api } from 'lib/api'
+
+export const homeID = api.HOME_ID
+
+// TODO continue to add in fetch links as needed for slices and page content here
+export const pageFetchLinks = ['page.page_slug', 'page.parent_page']
--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,25 +1,54 @@
 import Prismic from 'prismic-javascript'
 import { PrismicClient } from 'lib/api'
 import Page from 'components/Page'
+import { getPaths } from 'lib/pathFormation'
+import { homeID, pageFetchLinks } from 'constants/page'
+import { getPageSlug } from 'lib/pageSlug'
 
 export default Page
 
 export async function getStaticProps({ params, preview = false, previewData }) {
   const { masterRef } = await PrismicClient.getApi()
   const ref = previewData?.ref || masterRef.ref
-  const slug = params.slug?.length ? `/${params.slug.join('/')}/` : '/'
+
+  // Get pages based on ending slug
+  const slugArray = params.slug
+  const slug = slugArray?.length ? slugArray[slugArray.length - 1] : 'home'
+
   const { results } = await PrismicClient.query(
-    Prismic.Predicates.at('my.page.slug', slug),
+    Prismic.Predicates.at('my.page.page_slug', slug),
     {
-      fetchLinks: ['page.slug'],
+      fetchLinks: pageFetchLinks,
       ref,
     }
   )
+
+  // Prismic only allows goes one-level deep with content relationships, so we need to perform additional look ups for nested pages
+  // Go through results if there are parent pages
+  let filteredPage = null
+  if (results && results.length) {
+    const curSlug = slugArray.join('/')
+    const resolvedPages = await Promise.all(
+      results.map(async (result) => {
+        // Determine result's full slug based on parent pages
+        const resultParentPages = await getPageSlug(result)
+
+        // Compare the result's parent slugs to the actual url
+        const isMatch = resultParentPages.join('/') === curSlug
+        return isMatch ? result : null
+      })
+    )
+
+    filteredPage = resolvedPages.find((page) => page !== null)
+  }
+
+  const page = filteredPage
+
+  // Get global layout items
   const { data: header } = await PrismicClient.getSingle('header', {
-    fetchLinks: ['page.slug'],
+    fetchLinks: ['page.page_slug', 'page.parent_page'],
     ref,
   })
-  const page = results[0]
 
   return {
     props: {
@@ -32,14 +61,21 @@ export async function getStaticProps({ params, preview = false, previewData }) {
 }
 
 export async function getStaticPaths() {
-  const { results: allPages } = await PrismicClient.query(
-    Prismic.Predicates.at('document.type', 'page')
+  // TODO handle results exceeding 100
+  const {
+    results: allPages,
+    total_pages: totalPages,
+  } = await PrismicClient.query(
+    Prismic.Predicates.at('document.type', 'page'),
+    {
+      pageSize: 100,
+      fetchLinks: pageFetchLinks,
+    }
   )
-  const paths = allPages
-    ?.filter((page) => page.data.slug !== '/')
-    .map((page) => ({
-      params: { slug: page.data.slug.split('/').filter((path) => path !== '') },
-    }))
+
+  // Form URLs based on slug + parent pages
+  const filteredPages = allPages?.filter((page) => page?.id !== homeID) // remove home page from paths
+  const paths = await getPaths({ pageType: 'page', pages: filteredPages })
 
   return {
     paths,
