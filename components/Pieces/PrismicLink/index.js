2e90858fbff672e58934216b95d29f9b005959cb
bretmorris <bret@useallfive.com>
2021-04-29 17:29:01 -0600

    simplify PrismicLink

--- components/Pieces/PrismicLink/index.js
+++ components/Pieces/PrismicLink/index.js
@@ -1,148 +1,61 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import classNames from 'classnames'
+import cn from 'classnames'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
 import { linkResolver } from 'lib/resolvers'
 import prismicLinkShape from 'shapes/prismic/link'
 import styles from './index.module.scss'
+import { Link as L } from 'prismic-reactjs'
 
+// Passes Prismic link objects into next/link
+// Can handle links to Documents and Web
 const PrismicLink = ({
-  className,
+  activeIsExact, // Defaults to matching all parents of current route as active
   activeClassName,
+  className,
   link = {},
-  nextLink,
-  children,
-}) => {
-  const { link_type, url } = link
-  const linkProps = { className, activeClassName, link, nextLink, children }
-
-  if (link_type === 'Document' || nextLink) {
-    return <DocumentLink {...linkProps} />
-  } else if (
-    link_type === 'Web' &&
-    url &&
-    url.indexOf('https://anchor') !== -1
-  ) {
-    return (
-      <JumpLink
-        className={className}
-        link={link}
-        activeClassName={activeClassName}>
-        {children}
-      </JumpLink>
-    )
-  } else if (
-    link_type === 'Web' &&
-    url &&
-    url.indexOf('https://anchor') === -1
-  ) {
-    return (
-      <RegularLink className={className} link={link}>
-        {children}
-      </RegularLink>
-    )
-  } else {
-    return <NoLink className={className}>{children}</NoLink>
-  }
-}
-
-const prismicLinkPropTypes = {
-  className: PropTypes.string,
-  activeClassName: PropTypes.string,
-  link: PropTypes.oneOfType([prismicLinkShape, PropTypes.string]),
-  nextLink: PropTypes.bool,
-  children: PropTypes.node,
-}
-
-PrismicLink.propTypes = prismicLinkPropTypes
-
-export default PrismicLink
-
-const DocumentLink = ({
-  className,
-  activeClassName,
-  nextLink,
-  link,
   children,
 }) => {
   let isActive
-  const href = nextLink ? link : linkResolver(link)
+  const href = L.url(link, linkResolver)
+
+  // Determine if link matches current route
   if (useRouter()) {
     const { asPath } = useRouter()
     const i = asPath.indexOf('?')
     const path = i > 0 ? `${asPath.substring(0, i)}/` : `${asPath}/`
-    isActive = path.indexOf(href) > -1
+    const LEADING_AND_TRAILING_SLASH_REGEX = /^\/|\/$/g
+    const trimmedPath = path.replace(LEADING_AND_TRAILING_SLASH_REGEX, '')
+    const trimmedHref = href.replace(LEADING_AND_TRAILING_SLASH_REGEX, '')
+    isActive = activeIsExact
+      ? trimmedPath === trimmedHref
+      : trimmedPath.indexOf(trimmedHref) > -1
   }
+
   return (
-    <Link href={href}>
+    <Link {...{ href }}>
       <a
-        className={classNames(styles.link, className, {
+        className={cn(styles.link, className, {
           [activeClassName]: isActive,
           [styles.linkActive]: isActive,
-        })}>
+        })}
+        rel={link.link_type === 'Web' && 'noreferrer'}
+        target={link.target}>
         {children}
       </a>
     </Link>
   )
 }
 
-DocumentLink.propTypes = prismicLinkPropTypes
-
-const RegularLink = ({ className, children, link }) => {
-  const { url, target } = link || {}
-  return (
-    <a
-      className={classNames(styles.link, className)}
-      href={url}
-      target={target}
-      rel={target === '_blank' ? 'noreferrer' : 'false'}>
-      {children}
-    </a>
-  )
-}
-
-RegularLink.propTypes = {
-  className: PropTypes.string,
-  link: PropTypes.oneOfType([prismicLinkShape, PropTypes.string]),
-  children: PropTypes.node,
-}
-
-const NoLink = ({ className, children }) => {
-  return <span className={className}>{children}</span>
-}
-
-NoLink.propTypes = {
-  className: PropTypes.string,
-  children: PropTypes.node,
-}
-
-const JumpLink = ({ className, link, children, activeClassName }) => {
-  const { url } = link
-  const router = useRouter()
-  const href = url?.split('https://anchor:')[1]
-  const jump = (e) => {
-    e.preventDefault()
-    router.push(href, undefined)
-  }
-  const isActive = router.asPath.replace('?', '/?') === href
-
-  return (
-    <a
-      onClick={jump}
-      className={classNames(styles.link, className, {
-        [activeClassName]: isActive,
-        [styles.linkActive]: isActive,
-      })}
-      href={href}>
-      {children}
-    </a>
-  )
-}
-
-JumpLink.propTypes = {
-  className: PropTypes.string,
+const prismicLinkPropTypes = {
   activeClassName: PropTypes.string,
-  link: PropTypes.oneOfType([prismicLinkShape, PropTypes.string]),
+  activeIsExact: PropTypes.bool,
+  className: PropTypes.string,
+  link: prismicLinkShape,
   children: PropTypes.node,
 }
+
+PrismicLink.propTypes = prismicLinkPropTypes
+
+export default PrismicLink
