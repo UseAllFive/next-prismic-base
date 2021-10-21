5e7d1dd8b0637b04f03de8868f518f7d65dcb7fd
Ryan Gordon <ryan.gordon@useallfive.com>
2021-10-21 14:28:41 -0700

    Add default htmlSerializer, rich text helper for removing special characters

--- components/Slices/RichText/index.js
+++ components/Slices/RichText/index.js
@@ -1,11 +1,12 @@
 import { RichText } from 'prismic-reactjs'
 import styles from './index.module.scss'
 import prismicRichTextShare from 'shapes/prismic/richtext'
+import { htmlSerializer } from 'lib/richtext'
 
 const RichTextComponent = ({ richtext }) => {
   return (
     <div className={styles.container}>
-      <RichText render={richtext} />
+      <RichText render={richtext} htmlSerializer={htmlSerializer} />
     </div>
   )
 }
